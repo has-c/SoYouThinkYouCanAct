85a9a3f91a7db4fe67ab518fe9bfd89ad6cab874
Hasnain Cheena <you@example.com>
2017-07-18 22:50:46 +1200

    working camera functionality

---
+++ Game/Game/Game.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Game.Droid.MainApplication, Game.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5f64c5ef8a7a5380f1662297587b77ad5.MainApplication.class, md5f64c5ef8a7a5380f1662297587b77ad5.MainApplication.__md_methods);
+		
+	}
+}
